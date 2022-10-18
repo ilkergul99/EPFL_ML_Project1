79a708c1f0981d77b362d30a259bb5fda453544e
cankirimca <cankirmca@gmail.com>
2022-10-18 16:38:34 +0200

    added gd and sgd implementations

--- implementations.py
+++ implementations.py
@@ -1,5 +1,88 @@
 import numpy as np
 
+"""returns the mean squared error"""
+def compute_mse(e):
+    sq = np.square(e)
+    error = 0.5*np.mean(sq)
+    return error
+
+"""returns the gradient"""
+def compute_gradient(y, tx, w):
+    e = y - np.dot(tx,w)
+    g = -np.dot(tx.T,e)/(len(y))
+    return g,e
+
+"""returns the gradient for sgd, same as the previous function"""
+def compute_stoch_gradient(y, tx, w):
+    e = y - np.dot(tx,w)
+    g = -np.dot(tx.T,e)/(len(y))
+    return g,e
+
+"""performs gradient descent"""
+def gradient_descent(y, tx, w_init, max_iters, gamma):
+    # Define parameters to store w and loss
+    ws = [w_init]
+    losses = []
+    w = w_init
+    
+    for n_iter in range(max_iters):
+        #compute loss and gradient
+        grad,e = compute_gradient(y,tx,w)
+        loss = compute_mse(e)
+        
+        #update the weights
+        w = w - gamma*grad
+        ws.append(w)
+        losses.append(loss)
+        print("GD iter. {bi}/{ti}: loss={l}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss))  
+    return losses, ws
+
+"""the copy of the function in helpers.py, should we change this?"""
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+"""performs stochastic gradient descent"""
+def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    
+    
+    for n_iter in range(max_iters):
+        for yn,txn in batch_iter(y,tx,batch_size,1):
+            grad, e = compute_stoch_gradient(yn, txn, w)
+            w = w - gamma*grad
+            loss = compute_mse(e)
+            ws.append(w)
+            losses.append(loss)
+            
+        print("SGD iter. {bi}/{ti}: loss={l}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss))        
+    return losses, ws
+
 def sigmoid(t):
     """apply the sigmoid function on t"""
     return 1 / (1 + np.exp(-t))
