0008175377a8a5e7f41c59430bf2431f238a146b
İlker Gül <ilkergul@sabanciuniv.edu>
2022-10-19 22:34:02 +0200

    Necessary differentiation is added

--- implementations.py
+++ implementations.py
@@ -1,23 +1,7 @@
 import numpy as np
-
+from helper_functions_project1 import *
 """""""""""""""""""""""""""load data start"""""""""""""""""""""""""""""""
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-def load_data(path):
-    #alternative read at once and then split into x and y
-    X = np.genfromtxt(
-    path, delimiter=",", skip_header=1, usecols=range(2,32))
-    Y = np.genfromtxt(
-    path, delimiter=",", skip_header=1, converters={1: lambda x: 0 if b's' in x else 1}, usecols=[1])
-    return X,Y
-
-def standardize(x):
-    """Standardize the original data set."""
-    mean_x = np.mean(x)
-    x = x - mean_x
-    std_x = np.std(x)
-    x = x / std_x
-    return x, mean_x, std_x
-
 #height, weight, gender = load_data()
 #x, mean_x, std_x = standardize(height)
 #y, tx = build_model_data(x, weight)
@@ -28,35 +12,28 @@ tx = np.c_[np.ones(len(y)), x]
 """""""""""""""""""""""""""load data end"""""""""""""""""""""""""""""""
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-"""returns the mean squared error"""
-def compute_mse(e):
-    sq = np.square(e)
-    error = 0.5*np.mean(sq)
-    return error
-
-"""returns the gradient"""
-def compute_gradient(y, tx, w):
-    e = y - np.dot(tx,w)
-    g = -np.dot(tx.T,e)/(len(y))
-    return g,e
-
-"""returns the gradient for sgd, same as the previous function"""
-def compute_stoch_gradient(y, tx, w):
-    e = y - np.dot(tx,w)
-    g = -np.dot(tx.T,e)/(len(y))
-    return g,e
-
 """performs gradient descent"""
-def gradient_descent(y, tx, w_init, max_iters, gamma):
-    # Define parameters to store w and loss
-    ws = [w_init]
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """The Gradient Descent (GD) algorithm. 
+    Args:
+        y: Given labels of data = (N,)
+        tx: Features of the data = (N,D)
+        initial_w: numpy array of shape=(D, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+        
+    Returns:
+        loss[-1]: a list of length max_iters containing the loss value (scalar) for each iteration of GD
+        ws[-1]: a list of length max_iters containing the model parameters as numpy arrays of shape (D, ), for each iteration of GD 
+    """
+    ws = [initial_w]
     losses = []
-    w = w_init
+    w = initial_w
     
     for n_iter in range(max_iters):
         #compute loss and gradient
-        grad,e = compute_gradient(y,tx,w)
-        loss = compute_mse(e)
+        grad = compute_gradient(y,tx,w)
+        loss = compute_loss_mse(y,tx,w)
         
         #update the weights
         w = w - gamma*grad
@@ -66,39 +43,11 @@ def gradient_descent(y, tx, w_init, max_iters, gamma):
               bi=n_iter, ti=max_iters - 1, l=loss))  
     return losses, ws
 
-"""the copy of the function in helpers.py, should we change this?"""
-def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
-    """
-    Generate a minibatch iterator for a dataset.
-    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
-    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
-    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
-    Example of use :
-    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
-        <DO-SOMETHING>
-    """
-    data_size = len(y)
-
-    if shuffle:
-        shuffle_indices = np.random.permutation(np.arange(data_size))
-        shuffled_y = y[shuffle_indices]
-        shuffled_tx = tx[shuffle_indices]
-    else:
-        shuffled_y = y
-        shuffled_tx = tx
-    for batch_num in range(num_batches):
-        start_index = batch_num * batch_size
-        end_index = min((batch_num + 1) * batch_size, data_size)
-        if start_index != end_index:
-            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
-
 """performs stochastic gradient descent"""
-def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
     ws = [initial_w]
     losses = []
     w = initial_w
-    
-    
     for n_iter in range(max_iters):
         for yn,txn in batch_iter(y,tx,batch_size,1):
             grad, e = compute_stoch_gradient(yn, txn, w)
@@ -111,24 +60,52 @@ def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
                 bi=n_iter, ti=max_iters - 1, l=loss))        
     return losses, ws
 
-def sigmoid(t):
-    """apply the sigmoid function on t"""
-    return 1 / (1 + np.exp(-t))
+def least_squares(y, tx):
+    """
+    Least squares regression using normal equations
+    Args:
+        y: Given labels of data = (N,)
+        tx: Features of the data = (N,D)
+    Returns:
+        w: optimized weight vector for the model
+        loss: optimized final loss based on mean squared error
+    """
+    Gram_matrix = tx.T.dot(tx) #shape = (D,D)
+    second_matrix = tx.T.dot(y) #shape = (D,1)
+    w = np.linalg.solve(Gram_matrix, second_matrix) #shape = (D,1)
+    loss = compute_loss_mse(y, tx, w)
+    return w, loss
 
-def negative_likelihood_loss(y, tx, w):
-    """returns loss"""
-    t = np.dot(tx, w)
-    return np.sum(np.log(1 + np.exp(t)) - y * t) 
-
-def negative_likelihood_grad(y, tx, w):
-    """returns grad"""
-    return np.dot(tx.T, sigmoid(np.dot(tx, w)) - y)
-
-def reg_negative_likelihood_grad(y, tx, w, lambda_):
-    """returns grad with regularization term for l2"""
-    return np.dot(tx.T, sigmoid(np.dot(tx, w)) - y) + 2 * lambda_ * w
+def ridge_regression(y, tx, lambda_):
+    """
+    Ridge regression using normal equations
+    Args:
+        y: Given labels of data = (N,)
+        tx: Features of the data = (N,D)
+        lambda_: regularization parameter
+    Returns:
+        w: optimized weight vector for the model
+        loss: optimized final loss based on mean squared error
+    """
+    ridge_matrix = tx.T.dot(tx) + 2 * y.shape[0] * lambda_ * np.identity(tx.shape[1]) #shape = (D,D)
+    second_matrix = tx.T.dot(y) #shape = (D,1)
+    w = np.linalg.solve(ridge_matrix, second_matrix) #shape = (D,1)
+    loss = compute_loss_mse(y, tx, w)
+    return w, loss
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    """
+    Logistic Regression using Gradient Descent
+    Args:
+        y: Given labels of data = (N,)
+        tx: Features of the data = (N,D)
+        initial_w: numpy array of shape=(D, ). The initial guess (or the initialization) for the model parameters
+        max_iters: number of steps to run
+        gamma: a scalar denoting the total number of iterations 
+    Returns:
+        w: optimized weight vector for the model
+        loss: optimized final loss based on logistic loss
+    """
     w = initial_w.copy()
     
     for _ in range(max_iters):
@@ -140,6 +117,19 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
     return w, negative_likelihood_loss(y, tx, w)
 
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    """
+    Regularized Logistic Regression using Gradient Descent 
+    Args:
+        y: Given labels of data = (N,)
+        tx: Features of the data = (N,D)
+        lambda_: regularization parameter
+        initial_w: numpy array of shape=(D, ). The initial guess (or the initialization) for the model parameters
+        max_iters: number of steps to run
+        gamma: a scalar denoting the total number of iterations 
+    Returns:
+        w: optimized weight vector for the model
+        loss: optimized final loss based on logistic loss
+    """
     w = initial_w.copy()
     
     for _ in range(max_iters):
